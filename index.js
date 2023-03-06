dd0e267c0b2e18815839d124cd393796d9a97e4c
HomecheckYJH <road2023@homecheck.kr>
2023-03-06 18:08:00 +0900

    21

--- index.js
+++ index.js
@@ -14,13 +14,13 @@
 const fs = require('fs')
 const path = require('path')
 
-// const appPath = path.resolve(__dirname, '../../src/main.js')
+const appPath = path.resolve(__dirname, '../src/main.js')
 //여기서 지금 자꾸 cache 디렉토리에 접근한다.
 
-// let appFile = fs.readFileSync(appPath, { encoding: 'utf-8' })
-let appFile = fs.readFileSync('../../src/main.js', { encoding: 'utf-8' })
+let appFile = fs.readFileSync(appPath, { encoding: 'utf-8' })
+// let appFile = fs.readFileSync('../../src/main.js', { encoding: 'utf-8' })
 
 if (!appFile.includes("../node_modules/yjhlib/css/globalcss.css")) {
   appFile = `import "../node_modules/yjhlib/css/globalcss.css";\n` + appFile
-  fs.writeFileSync('../../src/main.js', appFile, { encoding: 'utf-8' })
+  fs.writeFileSync(appPath, appFile, { encoding: 'utf-8' })
 }
