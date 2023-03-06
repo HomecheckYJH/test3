6aef4cb25d075514852450c78e1d790ce3f385b2
HomecheckYJH <road2023@homecheck.kr>
2023-03-06 17:47:30 +0900

    17

--- index.js
+++ index.js
@@ -14,8 +14,7 @@
 const fs = require('fs')
 const path = require('path')
 
-// const appPath = path.resolve(__dirname, '../../src/main.js')
-const appPath = path.resolve(__dirname, '@/main.js')
+const appPath = path.resolve(__dirname, '../../src/main.js')
 
 let appFile = fs.readFileSync(appPath, { encoding: 'utf-8' })
 
