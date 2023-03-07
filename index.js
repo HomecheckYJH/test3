8e3c801b05547832594f58c6c8f412cec6f93758
HomecheckYJH <road2023@homecheck.kr>
2023-03-07 10:44:28 +0900

    31

--- index.js
+++ index.js
@@ -3,46 +3,46 @@ import { createApp } from 'vue'
 
 const app = createApp({})
 
-import { Accordion } from './components/Accordion.vue';
+// import { Accordion } from './components/Accordion.vue';
 import {AnimationSpinner} from './components/AnimationSpinner.vue';
-import {AnimationStatus} from './components/AnimationStatus.vue';
-import {Autocomplete} from './components/Autocomplete.vue';
-import {AutocompleteSelect} from './components/AutocompleteSelect.vue';
-import {Button} from './components/Button.vue';
-import {ButtonCard} from './components/ButtonCard.vue';
-import {ButtonIcon} from './components/ButtonIcon.vue';
-import {Calendar} from './components/Calendar.vue';
-import {Card} from './components/Card.vue';
-import {Context} from './components/Context.vue';
-import {Date} from './components/Date.vue';
-import {Dropdown} from './components/Dropdown.vue';
-import {DropdownFunctional} from './components/DropdownFunctional.vue';
-import {Filedrop} from './components/Filedrop.vue';
-import {Frame} from './components/Frame.vue';
-import {Grid} from './components/Grid.vue';
-import {Hint} from './components/Hint.vue';
-import {HintWrap} from './components/HintWrap.vue';
-import {InputNumberpad} from './components/InputNumberpad.vue';
-import {List} from './components/List.vue';
-import {ListItem} from './components/ListItem.vue';
-import {Logo} from './components/Logo.vue';
-import {Markdown} from './components/Markdown.vue';
-import {OverlayDrawer} from './components/OverlayDrawer.vue';
-import {OverlayDrawerFrame} from './components/OverlayDrawerFrame.vue';
-import {OverlayIndicator} from './components/OverlayIndicator.vue';
-import {OverlayModal} from './components/OverlayModal.vue';
-import {OverlayNotifications} from './components/OverlayNotifications.vue';
-import {Placeholder} from './components/Placeholder.vue';
-import {Quote} from './components/Quote.vue';
-import {RadioGroupBtn} from './components/RadioGroupBtn.vue';
-import {Step} from './components/Step.vue';
-import {Tab} from './components/Tab.vue';
-import {TabContent} from './components/TabContent.vue';
-import {Tag} from './components/Tag.vue';
-import {Toggle} from './components/Toggle.vue';
-import {ToggleMode} from './components/ToggleMode.vue';
-import {TranshesCheckAccordion} from './components/TranshesCheckAccordion.vue';
-import {TransitionExpand} from './components/TransitionExpand.vue';
+// import {AnimationStatus} from './components/AnimationStatus.vue';
+// import {Autocomplete} from './components/Autocomplete.vue';
+// import {AutocompleteSelect} from './components/AutocompleteSelect.vue';
+// import {Button} from './components/Button.vue';
+// import {ButtonCard} from './components/ButtonCard.vue';
+// import {ButtonIcon} from './components/ButtonIcon.vue';
+// import {Calendar} from './components/Calendar.vue';
+// import {Card} from './components/Card.vue';
+// import {Context} from './components/Context.vue';
+// import {Date} from './components/Date.vue';
+// import {Dropdown} from './components/Dropdown.vue';
+// import {DropdownFunctional} from './components/DropdownFunctional.vue';
+// import {Filedrop} from './components/Filedrop.vue';
+// import {Frame} from './components/Frame.vue';
+// import {Grid} from './components/Grid.vue';
+// import {Hint} from './components/Hint.vue';
+// import {HintWrap} from './components/HintWrap.vue';
+// import {InputNumberpad} from './components/InputNumberpad.vue';
+// import {List} from './components/List.vue';
+// import {ListItem} from './components/ListItem.vue';
+// import {Logo} from './components/Logo.vue';
+// import {Markdown} from './components/Markdown.vue';
+// import {OverlayDrawer} from './components/OverlayDrawer.vue';
+// import {OverlayDrawerFrame} from './components/OverlayDrawerFrame.vue';
+// import {OverlayIndicator} from './components/OverlayIndicator.vue';
+// import {OverlayModal} from './components/OverlayModal.vue';
+// import {OverlayNotifications} from './components/OverlayNotifications.vue';
+// import {Placeholder} from './components/Placeholder.vue';
+// import {Quote} from './components/Quote.vue';
+// import {RadioGroupBtn} from './components/RadioGroupBtn.vue';
+// import {Step} from './components/Step.vue';
+// import {Tab} from './components/Tab.vue';
+// import {TabContent} from './components/TabContent.vue';
+// import {Tag} from './components/Tag.vue';
+// import {Toggle} from './components/Toggle.vue';
+// import {ToggleMode} from './components/ToggleMode.vue';
+// import {TranshesCheckAccordion} from './components/TranshesCheckAccordion.vue';
+// import {TransitionExpand} from './components/TransitionExpand.vue';
 
 // app.component('Accordion', Accordion);
 app.component('AnimationSpinner', AnimationSpinner);
@@ -87,4 +87,4 @@ app.component('AnimationSpinner', AnimationSpinner);
 
 export { AnimationSpinner };
 
-Accordion, AnimationSpinner, AnimationStatus, Autocomplete, AutocompleteSelect, Button, ButtonCard, ButtonIcon, Calendar, Card, Context, Date, Dropdown, DropdownFunctional, Filedrop, Frame, Grid, Hint, HintWrap, InputNumberpad, List, ListItem, Logo, Markdown, OverlayDrawer, OverlayDrawerFrame, OverlayIndicator, OverlayModal, OverlayNotifications, Placeholder, Quote, RadioGroupBtn, Step, Tab, TabContent, Tag, Toggle, ToggleMode, TranshesCheckAccordion, TransitionExpand
+// Accordion, AnimationSpinner, AnimationStatus, Autocomplete, AutocompleteSelect, Button, ButtonCard, ButtonIcon, Calendar, Card, Context, Date, Dropdown, DropdownFunctional, Filedrop, Frame, Grid, Hint, HintWrap, InputNumberpad, List, ListItem, Logo, Markdown, OverlayDrawer, OverlayDrawerFrame, OverlayIndicator, OverlayModal, OverlayNotifications, Placeholder, Quote, RadioGroupBtn, Step, Tab, TabContent, Tag, Toggle, ToggleMode, TranshesCheckAccordion, TransitionExpand
