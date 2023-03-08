5b24001e14f0b9b573bbddd8d21375e9c729b1ef
HomecheckYJH <road2023@homecheck.kr>
2023-03-08 12:56:35 +0900

    logo, filedrop

--- components/ButtonIcon.vue
+++ components/ButtonIcon.vue
@@ -1,36 +1,36 @@
 <template>
     <div class="icon-wrapper" style="position:relative" :class="[align]">
-    <i v-wave class="bx" :class="[`bx-${toIcon(icon)}`]"></i>
+        <i v-wave class="bx" :class="[`bx-${toIcon(icon)}`]"></i>
     </div>
 </template>
 <script>
 export default {
-    props:['align','icon'],
-    setup(){
-        function toIcon(icon){
-            switch(icon){
+    props: ['align', 'icon'],
+    setup() {
+        function toIcon(icon) {
+            switch (icon) {
                 case 'close':
-                return 'x'
+                    return 'x'
                 case 'edit':
-                return 'edit-alt'
+                    return 'edit-alt'
                 case 'delete':
-                return 'trash'
-                default :
-                return icon
+                    return 'trash'
+                default:
+                    return icon
             }
         }
-        return {toIcon  }
+        return { toIcon }
     }
 }
 </script>
 <style scoped>
-.icon-wrapper{
-    width:36px;
-    height:36px;
+.icon-wrapper {
+    width: 36px;
+    height: 36px;
 }
 
 
-.icon-wrapper i{
+.icon-wrapper i {
     font-size: 24px;
     position: absolute;
     right: 0;
@@ -39,19 +39,16 @@ export default {
     display: flex;
     align-items: center;
     justify-content: center;
-    transform: translate(0,0%);
+    transform: translate(0, 0%);
     border-radius: 100%;
     transition: background 0.3s ease;
 }
 
-.icon-wrapper.center i{
-      transform: translate(0,-2px);
+.icon-wrapper.center i {
+    transform: translate(0, -2px);
 }
-.icon-wrapper i:hover
-{
+
+.icon-wrapper i:hover {
     background-color: var(--button-hover);
 }
-
-
-
 </style>
