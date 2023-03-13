93d79d14c5d456d46df69dfa3ae0d128522c26b2
HomecheckYJH <road2023@homecheck.kr>
2023-03-13 10:42:09 +0900

    numberpad

--- components/Logo.vue
+++ components/Logo.vue
@@ -2,7 +2,7 @@
   <div class="d-f hck-logo-component">
     <!-- <img class="hck-logo" src="../assets/img/logo_lg.png"> -->
     <!-- <img :src="Image" alt="image"> -->
-    <img src="../assets/img/logo_lg.png" alt="My Image" class="hck-logo">
+    <img src="../assets/img/logo_lg.png" alt="My Image">
     
       
     <div class="logo-text">
